ced079b8e621423073d0ecf587900ca7ea670a62
Christian <c.schawe@web.de>
2020-01-22 10:36:36 +0100

    bug fixing

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -7,13 +7,12 @@ public class GravityManager : MonoBehaviour
     
     private void OnTriggerStay(Collider other)
     {
-        if(other.gameObject.tag =="Apple")
+        if(other.gameObject.tag =="Apple" || other.gameObject.tag =="Pear" || other.gameObject.tag =="Peach")
         {
             if(OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) >= 0.8f || OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) >= 0.8f)
             {
                 
                 other.attachedRigidbody.useGravity = true;
-                other.attachedRigidbody.isKinematic = false;
             }
         }     
     }
